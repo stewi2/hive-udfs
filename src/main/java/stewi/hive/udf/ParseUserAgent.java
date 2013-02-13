f33c12644efcd0148bb2658ed0cccaac10fff0fd
Stefan Will <swill@zendesk.com>
2013-02-13 17:09:47 -0500

    Updated ParseUserAgent UDF to output full set of values from the underlying parser

--- src/main/java/stewi/hive/udf/ParseUserAgent.java
+++ src/main/java/stewi/hive/udf/ParseUserAgent.java
@@ -3,7 +3,10 @@ package stewi.hive.udf;
 import java.util.HashMap;
 import java.util.Map;
 
+import nl.bitwalker.useragentutils.Browser;
+import nl.bitwalker.useragentutils.OperatingSystem;
 import nl.bitwalker.useragentutils.UserAgent;
+import nl.bitwalker.useragentutils.Version;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
@@ -20,10 +23,21 @@ public class ParseUserAgent extends UDF {
 		Map<String,String> result = new HashMap<String,String>();
 
 		UserAgent ua = UserAgent.parseUserAgentString(input.toString());
-		result.put("browser",ua.getBrowser().toString());
+		
 		result.put("os",ua.getOperatingSystem().toString());
-		result.put("browsertype", ua.getBrowser().getBrowserType().toString());
-		result.put("engine", ua.getBrowser().getRenderingEngine().toString());
+		result.put("os.name", ua.getOperatingSystem().getName());
+		result.put("os.manufacturer", ua.getOperatingSystem().getManufacturer().getName());
+		result.put("os.device.type", ua.getOperatingSystem().getDeviceType().getName());
+		result.put("os.device.mobile", String.valueOf(ua.getOperatingSystem().isMobileDevice()));
+		
+		result.put("browser", ua.getBrowser().toString());
+		result.put("browser.manufacturer", ua.getBrowser().getManufacturer().getName());
+		result.put("browser.type", ua.getBrowser().getBrowserType().getName());
+		result.put("browser.engine", ua.getBrowser().getRenderingEngine().toString());
+		result.put("browser.version", ua.getBrowserVersion().getVersion());
+		result.put("browser.majorversion", ua.getBrowserVersion().getMajorVersion());
+		result.put("browser.minorversion", ua.getBrowserVersion().getMinorVersion());
+		result.put("browser.name", ua.getBrowser().getName());
 
 		return result;
 	}
--- src/test/java/stewi/hive/udf/ParseUserAgentTest.java
+++ src/test/java/stewi/hive/udf/ParseUserAgentTest.java
@@ -13,10 +13,20 @@ public class ParseUserAgentTest {
 	public void testEvaluate() {
 		ParseUserAgent udf = new ParseUserAgent();
 		Map<String,String> output = udf.evaluate(new Text("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; InfoPath.2; .NET4.0C; .NET4.0E)"));
+
 		assertEquals("IE8",output.get("browser"));
 		assertEquals("WINDOWS_XP",output.get("os"));
-		assertEquals("WEB_BROWSER",output.get("browsertype"));
-		assertEquals("TRIDENT",output.get("engine"));
+		assertEquals("Windows XP",output.get("os.name"));
+		assertEquals("Microsoft Corporation", output.get("os.manufacturer"));
+		assertEquals("Browser",output.get("browser.type"));
+		assertEquals("TRIDENT",output.get("browser.engine"));
+		assertEquals("false",output.get("os.device.mobile"));
+		assertEquals("Computer", output.get("os.device.type"));
+		assertEquals("8", output.get("browser.majorversion"));
+		assertEquals("0", output.get("browser.minorversion"));
+		assertEquals("8.0", output.get("browser.version"));
+		assertEquals("Internet Explorer 8", output.get("browser.name"));
+		assertEquals("Microsoft Corporation", output.get("browser.manufacturer"));
 	}
 
 }
